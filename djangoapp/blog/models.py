0bfe0180445009624fa098bdb76c39ddbf8018b0
Gustavo-Félix <gustavofelix2007@gmail.com>
2025-05-19 08:34:40 -0300

    Criando o model e admin Page (models.Model e admin.ModelAdmin do Django)

--- djangoapp/blog/models.py
+++ djangoapp/blog/models.py
@@ -20,6 +20,9 @@ class Tag(models.Model):
             self.slug = slugfy_new(self.name, 4)
         return super().save(*args, **kwargs)
     
+    def __str__(self) -> str:
+        return self.name
+    
 class Category(models.Model):
     class Meta:
         verbose_name = 'Category'
@@ -38,3 +41,30 @@ class Category(models.Model):
         if not self.slug:
             self.slug = slugfy_new(self.name, 4)
         return super().save(*args, **kwargs)
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Page(models.Model):
+    title = models.CharField(max_length=65)
+    slug = models.SlugField(
+        unique=True, 
+        default="",
+        null=False, 
+        blank=True, 
+        max_length=255,
+    )
+    is_published = models.BooleanField(
+        default=False,
+        help_text='Este campo precisará estar marcado para a página ser exibida!'
+        )
+    content = models.TextField()
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugfy_new(self.title, 4)
+        return super().save(*args, **kwargs)
+    
+    def __str__(self) -> str:
+        return self.title
