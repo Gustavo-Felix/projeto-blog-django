7a3578cdf9c103b149e60d1516457967fdf076d1
Gustavo-Félix <gustavofelix2007@gmail.com>
2025-05-20 09:13:51 -0300

    Configurando o django-summernote como editor HTML em TextField nos models Django

--- djangoapp/blog/admin.py
+++ djangoapp/blog/admin.py
@@ -1,5 +1,7 @@
 from blog.models import Category, Tag, Page, Post
 from django.contrib import admin
+from django_summernote.admin import SummernoteModelAdmin
+from .models import Post
 
 # Register your models here.
 
@@ -40,7 +42,8 @@ class PageAdmin(admin.ModelAdmin):
     }
 
 @admin.register(Post)
-class PostAdmin(admin.ModelAdmin):
+class PostAdmin(SummernoteModelAdmin):
+    summernote_fields = ('content',)
     list_display = 'id', 'title', 'is_published',  'created_by',
     list_display_links = 'title',
     search_fields = 'id', 'slug', 'title', 'excerpt', 'content',
